96fa02bcbb05969dc6fcddd317f1545af8dfc92a
Corey Calhoun <corey.calhoun@dkgconceptsllc.com>
2020-10-20 21:13:59 -0500

    --- Auto-Git Commit ---

--- src/requests.js
+++ src/requests.js
@@ -2,7 +2,7 @@
 const requests = {
     fetchGamesByPlatform: `/platforms?&language=en-US`,
     fetchGamesByGenres: `/genres?&language=en-US`,
-    fetchTopRated: `/games?2020-01-01,2019-12-31&ordering=-added&language=en-US`,
+    fetchTopRated: `/games?2019-01-01,2020-12-31&ordering=-added&language=en-US`,
 }
 
 export default requests;
